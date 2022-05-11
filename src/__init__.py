67df61d99fb01e841d3a73cd37f3b7f4b4538e0f
jbleyel <jbleyel@gmx.net>
2022-05-11 15:50:01 +0000

    remove change LANGUAGE env

--- src/__init__.py
+++ src/__init__.py
@@ -1,20 +1,17 @@
-﻿from Components.Language import language
-from Tools.Directories import resolveFilename, SCOPE_PLUGINS, SCOPE_LANGUAGE
-from os import environ as os_environ
-import gettext
+﻿from gettext import bindtextdomain, dgettext
 import sys
-
 from skin import loadSkin
+from Components.Language import language
+from Tools.Directories import resolveFilename, SCOPE_PLUGINS
+
 loadSkin("/usr/lib/enigma2/python/Plugins/Extensions/EnhancedMovieCenter/CoolSkin/EMCMediaCenter_LCD.xml")
 
 
 def localeInit():
-	lang = language.getLanguage()[:2] # getLanguage returns e.g. "fi_FI" for "language_country"
-	os_environ["LANGUAGE"] = lang # Enigma doesn't set this (or LC_ALL, LC_MESSAGES, LANG). gettext needs it!
-	gettext.bindtextdomain("EnhancedMovieCenter", resolveFilename(SCOPE_PLUGINS, "Extensions/EnhancedMovieCenter/locale"))
+	bindtextdomain("EnhancedMovieCenter", resolveFilename(SCOPE_PLUGINS, "Extensions/EnhancedMovieCenter/locale"))
 
 
-_ = lambda txt: gettext.dgettext("EnhancedMovieCenter", txt) if txt else ""
+_ = lambda txt: dgettext("EnhancedMovieCenter", txt) if txt else ""
 
 localeInit()
 language.addCallback(localeInit)
